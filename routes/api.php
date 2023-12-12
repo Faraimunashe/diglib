f51502832b3521d8748005fed7c817edb2ed6dc3
unknown <faraimunashe.m11@gmail.com>
2023-12-12 16:32:46 +0200

    added: an API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+
+    /* CHECK API STATUS */
+    Route::get('/status', function (Request $request) {
+        return response()
+            ->json(['version' => 'v1.0.0', 'up' => TRUE]);
+    });
+
+    /* API AUTHENTICATION ROUTES */
+    Route::post('login', [AuthenticationController::class, 'login']);
+    Route::post('register', [AuthenticationController::class, 'register']);
+
+    /* API PROTECTED ROUTES */
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+        Route::post('logout', [AuthenticationController::class, 'logout']);
+
+
+    });
+
 });
